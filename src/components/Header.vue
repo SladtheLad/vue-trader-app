cb35395656576f6ddf91d2230f399f3a92b1f958
Nick Sladden <nickoliasxii@gmail.com>
2018-10-31 11:17:22 -0500

    vuex next

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,34 @@
 <template>
-  <h1>HEADER</h1>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link class="navbar-brand" to="/">Stock Trader</router-link>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+        <router-link to="/portfolio" activeClass="active" tag="li">
+          <a class="nav-link">Portfolio</a>
+        </router-link>
+        <router-link to="/stocks" activeClass="active" tag="li">
+          <a class="nav-link">Stocks</a>
+        </router-link>
+      </ul>
+      <ul class="navbar-nav navbar-right">
+        <router-link to="/portfolio" activeClass="active" tag="li">
+          <a class="nav-link">End Day</a>
+        </router-link>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdownMenuLink"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >Save & Load</a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Save Data</a>
+            <a class="dropdown-item" href="#">Load Data</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>STOCKS</h1>
+  <div class="card-deck">
+    <app-stock v-for="stock in stocks" :key="stock" :stock="stock"></app-stock>
+  </div>
 </template>
+
+<script>
+import Stock from './Stock.vue'
+
+export default {
+  data() {
+    return { 
+      stocks: [
+        { id: 1, name: 'BMW', price: 200},
+        { id: 2, name: 'Google', price: 500},
+        { id: 3, name: 'Apple', price: 450},
+        { id: 4, name: 'Twitter', price: 300},
+      ]
+    }
+  },
+  components: {
+    appStock: Stock
+  }
+}
+</script>
+
+<style>
+.row {
+  margin-top: 10px;
+}
+</style>
+
